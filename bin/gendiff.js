2015f12b3c6d8cf712721d7ffef1a07c88bccbd8
Vyacheslav <kokorinv19@gmail.com>
2021-07-16 01:05:53 +0500

    modified gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,6 @@
 import { Command } from '../node_modules/commander/esm.mjs';
 const program = new Command();
 
-program.version('0.0.1');
+program.version('0.0.1').description('Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);
