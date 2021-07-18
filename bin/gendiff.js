d890b75073fd724cf6de6caed3665248be93184f
Vyacheslav <kokorinv19@gmail.com>
2021-07-19 00:00:44 +0500

    modified gendiff.js: add arguments and action()

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
 import { Command } from '../node_modules/commander/esm.mjs';
+import genDiff from '../src/index.js';
+import path from 'path';
+import { cwd } from 'process';
+
 const program = new Command();
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const filePathFirst = path.resolve(`${cwd()}`, '__fixtures__', `${filepath1}`);
+    const filePathSecond = path.resolve(`${cwd()}`, '__fixtures__', `${filepath2}`);
+    genDiff(filePathFirst, filePathSecond);
+  });
 
 program.parse(process.argv);
