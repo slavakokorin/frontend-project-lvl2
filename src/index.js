1c18e487bd2f73c7e29057455acfb58251516e81
Vyacheslav <kokorinv19@gmail.com>
2021-07-30 22:51:48 +0500

    move the definition of the path to the file into a separate function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import getObjectDifferences from './utilities.js';
 
 const getObject = (jsonContent) => JSON.parse(jsonContent);
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
 const genDiff = (filepath1, filepath2) => {
-  const jsonContent1 = fs.readFileSync(filepath1, 'utf-8');
-  const jsonContent2 = fs.readFileSync(filepath2, 'utf-8');
+  const jsonContent1 = readFile(filepath1);
+  const jsonContent2 = readFile(filepath2);
 
   const data1 = getObject(jsonContent1);
   const data2 = getObject(jsonContent2);
