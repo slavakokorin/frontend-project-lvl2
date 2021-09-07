85618ac1e0b964cebbc690c74a3ffc7130bb6b6e
Vyacheslav <kokorinv19@gmail.com>
2021-09-07 23:06:43 +0500

    added new test not flatted files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -38,3 +38,54 @@ test('comparing flat yaml files', () => {
 }`,
   );
 });
+
+test('comparing not flat json files', () => {
+  const readFirstFile = getFixturePath('file5.json');
+  const readSecondFile = getFixturePath('file6.json');
+  expect(genDiff(readFirstFile, '.json', readSecondFile, '.json')).toEqual(
+    `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`,
+  );
+});
