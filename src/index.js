76f7489ed44b47a0ab9be6242b6da42df7ea402e
Vyacheslav <kokorinv19@gmail.com>
2021-09-09 01:09:56 +0500

    modified

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import getObject from './parsers.js';
 import getObjectDifferences from './utilities.js';
 
-const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-const genDiff = (filepath1, fileExtension1, filepath2, fileExtension2) => {
-  const content1 = readFile(filepath1);
-  const сontent2 = readFile(filepath2);
+const genDiff = (filePath1, fileExtens1, filePath2, fileExtens2) => {
+  const content1 = readFile(filePath1);
+  const сontent2 = readFile(filePath2);
 
-  const data1 = getObject(content1, fileExtension1);
-  const data2 = getObject(сontent2, fileExtension2);
+  const data1 = getObject(content1, fileExtens1);
+  const data2 = getObject(сontent2, fileExtens2);
 
   return getObjectDifferences(data1, data2);
 };
