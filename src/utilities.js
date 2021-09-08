8b31d9d417a7bdea60abb8a43d07dde77b2d4c32
Vyacheslav <kokorinv19@gmail.com>
2021-09-09 01:10:39 +0500

    modified

--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
+import renderDiff from './stylish.js';
 
-const getAllKeys = (o1, o2) => {
+const getUniqueKeys = (o1, o2) => {
   const keys1 = _.keys(o1);
   const keys2 = _.keys(o2);
   const allKeys = _.union(keys1, keys2).sort();
@@ -8,86 +9,53 @@ const getAllKeys = (o1, o2) => {
 };
 
 const getObjectDifferences = (data1, data2) => {
-  const iter = (d1, d2) => {
-    const unic = getAllKeys(d1, d2);
-    const result = unic.map((key) => {
-      const diffInf = { name: key };
-      if (!_.has(d2, key) && !_.isObject(d1[key])) {
-        diffInf.condition = 'deleted';
-        diffInf.firstValue = d1[key];
-        diffInf.secondValue = 'no';
-      } else if (!_.has(d1, key) && !_.isObject(d2[key])) {
-        diffInf.condition = 'added';
-        diffInf.firstValue = 'no';
-        diffInf.secondValue = d2[key];
-      } else if (!_.isObject(d1[key]) && !_.isObject(d2[key]) && d1[key] === d2[key]) {
-        diffInf.firstValue = d1[key];
-        diffInf.secondValue = 'no';
+  const iter = (currentData1, currentData2) => {
+    const uniqueKeys = getUniqueKeys(currentData1, currentData2);
+    const result = uniqueKeys.map((key) => {
+      const diffInfo = { name: key };
+      if (!_.has(currentData2, key) && !_.isObject(currentData1[key])) {
+        diffInfo.condition = 'deleted';
+        diffInfo.firstValue = currentData1[key];
+      } else if (!_.has(currentData1, key) && !_.isObject(currentData2[key])) {
+        diffInfo.condition = 'added';
+        diffInfo.secondValue = currentData2[key];
       } else if (
-        (!_.isObject(d1[key]) || d1[key] === null) &&
-        (!_.isObject(d2[key]) || d2[key] === null) &&
-        d1[key] !== d2[key]
+        !_.isObject(currentData1[key]) &&
+        !_.isObject(currentData2[key]) &&
+        currentData1[key] === currentData2[key]
       ) {
-        diffInf.condition = 'changed';
-        diffInf.firstValue = d1[key];
-        diffInf.secondValue = d2[key];
-      } else if (_.isObject(d1[key]) && _.isObject(d2[key])) {
-        diffInf.nodeCondition = 'changed';
-        diffInf.condition = 'has children';
-        diffInf.children = iter(d1[key], d2[key]);
-      } else if (!_.isObject(d1[key]) && _.isObject(d2[key])) {
-        diffInf.nodeCondition = 'added';
-        diffInf.condition = 'has children';
-        diffInf.children = iter(d2[key], d2[key]);
-      } else if (!_.isObject(d2[key]) && _.isObject(d1[key])) {
-        if (!_.has(d2, key)) {
-          diffInf.condition = 'has children';
-          diffInf.children = iter(d1[key], d1[key]);
-        } else if (_.has(d1, key) || _.has(d2, key)) {
-          diffInf.secondValue = d2[key];
-          diffInf.condition = 'has children';
-          diffInf.children = iter(d1[key], d1[key]);
+        diffInfo.firstValue = currentData1[key];
+      } else if (
+        (!_.isObject(currentData1[key]) || currentData1[key] === null) &&
+        (!_.isObject(currentData2[key]) || currentData2[key] === null) &&
+        currentData1[key] !== currentData2[key]
+      ) {
+        diffInfo.condition = 'changed';
+        diffInfo.firstValue = currentData1[key];
+        diffInfo.secondValue = currentData2[key];
+      } else if (_.isObject(currentData1[key]) && _.isObject(currentData2[key])) {
+        diffInfo.nodeCondition = 'changed';
+        diffInfo.condition = 'has children';
+        diffInfo.children = iter(currentData1[key], currentData2[key]);
+      } else if (!_.isObject(currentData1[key]) && _.isObject(currentData2[key])) {
+        diffInfo.nodeCondition = 'added';
+        diffInfo.condition = 'has children';
+        diffInfo.children = iter(currentData2[key], currentData2[key]);
+      } else if (!_.isObject(currentData2[key]) && _.isObject(currentData1[key])) {
+        if (!_.has(currentData2, key)) {
+          diffInfo.condition = 'has children';
+          diffInfo.children = iter(currentData1[key], currentData1[key]);
+        } else if (_.has(currentData1, key) || _.has(currentData2, key)) {
+          diffInfo.secondValue = currentData2[key];
+          diffInfo.condition = 'has children';
+          diffInfo.children = iter(currentData1[key], currentData1[key]);
         }
       }
-      return diffInf;
+      return diffInfo;
     });
     return result;
   };
-
-  const stringify = (content, replacer, spacesCount) => {
-    const iterr = (currentVal, depth) => {
-      const firstSpace = replacer.repeat(spacesCount * depth);
-      const secondSpace = replacer.repeat(spacesCount * (depth - 1));
-      const resultString = ['{'];
-      _.mapValues(currentVal, (item) => {
-        if (item.condition === 'has children') {
-          if (item.nodeCondition === 'changed') {
-            resultString.push(`\n${firstSpace}${item.name}: ${iterr(item.children, depth + 1)}`);
-          } else if (item.nodeCondition === 'added') {
-            resultString.push(`\n${firstSpace.slice(2)}+ ${item.name}: ${iterr(item.children, depth + 1)}`);
-          } else {
-            resultString.push(`\n${firstSpace.slice(2)}- ${item.name}: ${iterr(item.children, depth + 1)}`);
-            if (_.has(item, 'secondValue')) {
-              resultString.push(`\n${firstSpace.slice(2)}+ ${item.name}: ${item.secondValue}`);
-            }
-          }
-        } else if (item.condition === 'changed') {
-          resultString.push(`\n${firstSpace.slice(2)}- ${item.name}: ${item.firstValue}`);
-          resultString.push(`\n${firstSpace.slice(2)}+ ${item.name}: ${item.secondValue}`);
-        } else if (item.condition === 'deleted') {
-          resultString.push(`\n${firstSpace.slice(2)}- ${item.name}: ${item.firstValue}`);
-        } else if (item.condition === 'added') {
-          resultString.push(`\n${firstSpace.slice(2)}+ ${item.name}: ${item.secondValue}`);
-        } else {
-          resultString.push(`\n${firstSpace.slice(2)}  ${item.name}: ${item.firstValue}`);
-        }
-      });
-      resultString.push(`\n${secondSpace}}`);
-      return resultString.join('');
-    };
-    return iterr(content, 1);
-  };
-  return stringify(iter(data1, data2), '  ', 2);
+  return renderDiff(iter(data1, data2), '  ', 2);
 };
 
 export default getObjectDifferences;
