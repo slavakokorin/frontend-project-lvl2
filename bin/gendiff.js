1639d72d7e3048735357c47de318da3df1eb15a5
Vyacheslav <kokorinv19@gmail.com>
2021-09-11 15:20:04 +0500

    modified gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,22 +4,24 @@ import path from 'path';
 import { cwd } from 'process';
 import { Command } from '../node_modules/commander/esm.mjs';
 import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 const getFilePath = (fileName) => path.resolve(`${cwd()}`, '__fixtures__', `${fileName}`);
-const getFileExtension = (fileName) => path.extname(fileName);
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
-  .action((fileName1, fileName2) => {
+  .arguments('<filepath1> <filepath2> [renderer]')
+  .action((fileName1, fileName2, render = stylish) => {
+    let currentRender = render;
+    if (render === undefined) {
+      currentRender = stylish;
+    }
     const filePath1 = getFilePath(fileName1);
     const filePath2 = getFilePath(fileName2);
-    const fileExtens1 = getFileExtension(fileName1);
-    const fileExtens2 = getFileExtension(fileName2);
-    console.log(genDiff(filePath1, fileExtens1, filePath2, fileExtens2));
+    console.log(genDiff(filePath1, filePath2, currentRender));
   });
 
 program.parse(process.argv);
