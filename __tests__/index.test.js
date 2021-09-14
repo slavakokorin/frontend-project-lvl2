220d9e8a4bf26528c8a953d38e9a99f2d59c1b18
Vyacheslav <kokorinv19@gmail.com>
2021-09-14 23:42:28 +0500

    modified

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,8 @@ import { test, expect } from '@jest/globals';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import stylish from '../src/stylish.js';
+import stylish from '../src/formatters/stylish.js';
+import plain from '../src/formatters/plain.js';
 
 // следующие две строки нужны для работы __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -88,5 +89,23 @@ test('comparing not flat json files', () => {
         fee: 100500
     }
 }`,
+  );
+});
+
+test('plain output', () => {
+  const readFirstFile = getFixturePath('file5.json');
+  const readSecondFile = getFixturePath('file6.json');
+  expect(genDiff(readFirstFile, readSecondFile, plain)).toEqual(
+    `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`,
   );
 });
