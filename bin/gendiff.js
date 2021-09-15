ea249a7692a2ec6db308cbdd99537971fa48508c
Vyacheslav <kokorinv19@gmail.com>
2021-09-15 22:31:56 +0500

    added selection of diff-formatters

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,7 @@ import path from 'path';
 import { cwd } from 'process';
 import { Command } from '../node_modules/commander/esm.mjs';
 import genDiff from '../src/index.js';
-import stylish from '../src/formatters/stylish.js';
-// import plain from '../src/formatters/plain.js';
+import getFormatter from '../src/formatters/index.js';
 
 const program = new Command();
 const getFilePath = (fileName) => path.resolve(`${cwd()}`, '__fixtures__', `${fileName}`);
@@ -13,12 +12,14 @@ const getFilePath = (fileName) => path.resolve(`${cwd()}`, '__fixtures__', `${fi
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2> [renderer]')
-  .action((fileName1, fileName2, render = stylish) => {
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((fileName1, fileName2) => {
+    const options = program.opts();
+    const formatter = getFormatter(options.format);
     const filePath1 = getFilePath(fileName1);
     const filePath2 = getFilePath(fileName2);
-    console.log(genDiff(filePath1, filePath2, render));
+    console.log(genDiff(filePath1, filePath2, formatter));
   });
 
 program.parse(process.argv);
