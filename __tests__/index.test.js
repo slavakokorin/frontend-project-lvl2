6d2fcd6938c88c6dfbddc5a29b0dad5a43542597
Vyacheslav <kokorinv19@gmail.com>
2021-09-18 21:00:25 +0500

    added new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -90,6 +90,76 @@ test('comparing not flat json files', () => {
   );
 });
 
+test('comparing not flat json files - 2', () => {
+  const readFirstFile = getFixturePath('file7.json');
+  const readSecondFile = getFixturePath('file8.json');
+  expect(genDiff(readFirstFile, readSecondFile, 'stylish')).toEqual(
+    `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: too much
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+    group4: {
+      - default: null
+      + default: 
+      - foo: 0
+      + foo: null
+      - isNested: false
+      + isNested: none
+      + key: false
+        nest: {
+          - bar: 
+          + bar: 0
+          - isNested: true
+        }
+      + someKey: true
+      - type: bas
+      + type: bar
+    }
+}`,
+  );
+});
+
 test('plain output', () => {
   const readFirstFile = getFixturePath('file5.json');
   const readSecondFile = getFixturePath('file6.json');
@@ -115,8 +185,7 @@ test('JSON output', () => {
     `[
   {
     "name": "common",
-    "nodeCondition": "changed",
-    "condition": "has children",
+    "nodeCondition": "not changed",
     "children": [
       {
         "name": "follow",
@@ -147,7 +216,9 @@ test('JSON output', () => {
       {
         "name": "setting5",
         "nodeCondition": "added",
-        "condition": "has children",
+        "secondValue": {
+          "key5": "value5"
+        },
         "children": [
           {
             "name": "key5",
@@ -158,13 +229,11 @@ test('JSON output', () => {
       },
       {
         "name": "setting6",
-        "nodeCondition": "changed",
-        "condition": "has children",
+        "nodeCondition": "not changed",
         "children": [
           {
             "name": "doge",
-            "nodeCondition": "changed",
-            "condition": "has children",
+            "nodeCondition": "not changed",
             "children": [
               {
                 "name": "wow",
@@ -190,8 +259,7 @@ test('JSON output', () => {
   },
   {
     "name": "group1",
-    "nodeCondition": "changed",
-    "condition": "has children",
+    "nodeCondition": "not changed",
     "children": [
       {
         "name": "baz",
@@ -206,11 +274,11 @@ test('JSON output', () => {
       },
       {
         "name": "nest",
+        "nodeCondition": "updated to str",
         "firstValue": {
           "key": "value"
         },
         "secondValue": "str",
-        "condition": "has children",
         "children": [
           {
             "name": "key",
@@ -224,7 +292,6 @@ test('JSON output', () => {
   {
     "name": "group2",
     "nodeCondition": "deleted",
-    "condition": "has children",
     "children": [
       {
         "name": "abc",
@@ -233,8 +300,7 @@ test('JSON output', () => {
       },
       {
         "name": "deep",
-        "nodeCondition": "changed",
-        "condition": "has children",
+        "nodeCondition": "not changed",
         "children": [
           {
             "name": "id",
@@ -248,17 +314,22 @@ test('JSON output', () => {
   {
     "name": "group3",
     "nodeCondition": "added",
-    "condition": "has children",
+    "secondValue": {
+      "deep": {
+        "id": {
+          "number": 45
+        }
+      },
+      "fee": 100500
+    },
     "children": [
       {
         "name": "deep",
-        "nodeCondition": "changed",
-        "condition": "has children",
+        "nodeCondition": "not changed",
         "children": [
           {
             "name": "id",
-            "nodeCondition": "changed",
-            "condition": "has children",
+            "nodeCondition": "not changed",
             "children": [
               {
                 "name": "number",
@@ -277,5 +348,31 @@ test('JSON output', () => {
     ]
   }
 ]`,
+  );
+});
+
+test('plain output - 2', () => {
+  const readFirstFile = getFixturePath('file7.json');
+  const readSecondFile = getFixturePath('file8.json');
+  expect(genDiff(readFirstFile, readSecondFile, 'plain')).toEqual(
+    `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to [complex value]
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From 'too much' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]
+Property 'group4.default' was updated. From null to ''
+Property 'group4.foo' was updated. From 0 to null
+Property 'group4.isNested' was updated. From false to 'none'
+Property 'group4.key' was added with value: false
+Property 'group4.nest.bar' was updated. From '' to 0
+Property 'group4.nest.isNested' was removed
+Property 'group4.someKey' was added with value: true
+Property 'group4.type' was updated. From 'bas' to 'bar'`,
   );
 });
