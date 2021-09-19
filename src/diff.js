259e96cfd42920b8c6d797c95e29c67b2c36a177
Vyacheslav <kokorinv19@gmail.com>
2021-09-19 23:47:15 +0500

    removed mutations, only const only hardcore

--- src/diff.js
+++ src/diff.js
@@ -12,39 +12,32 @@ const getObjectDifferences = (data1, data2) => {
   const iter = (currentData1, currentData2) => {
     const keys = getUniqueKeys(currentData1, currentData2);
     const result = keys.map((key) => {
-      // const diffInfo = { name: key };
       if (!_.has(currentData2, key)) {
-        if (!_.isObject(currentData1[key])) {
-          return {
+        return (!_.isObject(currentData1[key]))
+          ? {
             name: key,
             condition: 'deleted',
             firstValue: currentData1[key],
-          };
-        }
-        if (_.isObject(currentData1, key)) {
-          return {
+          }
+          : {
             name: key,
             nodeCondition: 'deleted',
             children: iter(currentData1[key], currentData1[key]),
           };
-        }
       }
       if (!_.has(currentData1, key)) {
-        if (!_.isObject(currentData2[key])) {
-          return {
+        return (!_.isObject(currentData2[key]))
+          ? {
             name: key,
             condition: 'added',
             secondValue: currentData2[key],
-          };
-        }
-        if (_.isObject(currentData2[key])) {
-          return {
+          }
+          : {
             name: key,
             nodeCondition: 'added',
             secondValue: currentData2[key],
             children: iter(currentData2[key], currentData2[key]),
           };
-        }
       }
       if (_.has(currentData1, key) && _.has(currentData2, key)) {
         if (_.isObject(currentData1[key]) && _.isObject(currentData2[key])) {
@@ -86,7 +79,6 @@ const getObjectDifferences = (data1, data2) => {
         firstValue: currentData1[key],
         secondValue: currentData2[key],
       };
-      // return diffInfo;
     });
     return result;
   };
