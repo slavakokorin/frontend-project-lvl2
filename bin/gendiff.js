7d7fe903b7230d00bb47be8637f55a7be3bcaa15
Vyacheslav <kokorinv19@gmail.com>
2021-09-22 23:33:40 +0500

    deleted comments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,26 +2,21 @@
 
 import path from 'path';
 import { cwd } from 'process';
-// import { Command } from '../node_modules/commander/esm.mjs';
 import commander from 'commander';
 import genDiff from '../src/index.js';
 
-// const program = new Command();
 const getFilePath = (fileName) => path.resolve(`${cwd()}`, '__fixtures__', `${fileName}`);
 
-// program
 commander
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((fileName1, fileName2) => {
-    // const options = program.opts();
     const options = commander.opts();
     const filePath1 = getFilePath(fileName1);
     const filePath2 = getFilePath(fileName2);
     console.log(genDiff(filePath1, filePath2, options.format));
   });
 
-// program.parse(process.argv);
 commander.parse(process.argv);
