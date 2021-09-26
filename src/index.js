396323a4f2fa639f485585924b8dd2bc1b0f726e
Vyacheslav <kokorinv19@gmail.com>
2021-09-27 01:04:27 +0500

    added working with absolut path

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
-import getObject from './parsers.js';
-import getObjectDifferences from './diff.js';
+import parseToObject from './parsers.js';
+import buildDiffTree from './diff.js';
 import getFormatter from './formatters/index.js';
 
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-const getFileExtension = (filePath) => path.extname(filePath);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const genDiff = (filePath1, filePath2, format) => {
+const getFilePath = (fileName) => path.resolve(__dirname, '..', '__fixtures__', `${fileName}`);
+
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const extractFormat = (filePath) => path.extname(filePath);
+
+const genDiff = (fileName1, fileName2, format) => {
+  const filePath1 = getFilePath(fileName1);
+  const filePath2 = getFilePath(fileName2);
   const content1 = readFile(filePath1);
   const сontent2 = readFile(filePath2);
-  const fileExtens1 = getFileExtension(filePath1);
-  const fileExtens2 = getFileExtension(filePath2);
-  const data1 = getObject(content1, fileExtens1);
-  const data2 = getObject(сontent2, fileExtens2);
-  const diff = getObjectDifferences(data1, data2);
-  const render = getFormatter(format);
-  return render(diff);
+  const firstFileFormat = extractFormat(filePath1);
+  const secondFileFormat = extractFormat(filePath2);
+  const dataForDifferences1 = parseToObject(content1, firstFileFormat);
+  const dataForDifferences2 = parseToObject(сontent2, secondFileFormat);
+  const diff = buildDiffTree(dataForDifferences1, dataForDifferences2);
+  const formatDiffTree = getFormatter(format);
+  return formatDiffTree(diff);
 };
 
 export default genDiff;
