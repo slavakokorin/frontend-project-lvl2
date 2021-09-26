8100841e96d1016f44a3709b91cb4a061c4fa158
Vyacheslav <kokorinv19@gmail.com>
2021-09-27 01:00:18 +0500

    remove unnecessary functionality

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import { cwd } from 'process';
 import commander from 'commander';
 import genDiff from '../src/index.js';
-
-const getFilePath = (fileName) => path.resolve(`${cwd()}`, '__fixtures__', `${fileName}`);
 
 commander
   .version('0.0.1')
@@ -14,9 +10,7 @@ commander
   .arguments('<filepath1> <filepath2>')
   .action((fileName1, fileName2) => {
     const options = commander.opts();
-    const filePath1 = getFilePath(fileName1);
-    const filePath2 = getFilePath(fileName2);
-    console.log(genDiff(filePath1, filePath2, options.format));
+    console.log(genDiff(fileName1, fileName2, options.format));
   });
 
 commander.parse(process.argv);
