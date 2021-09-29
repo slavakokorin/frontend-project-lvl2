86137941687ed58cff4462c643bc01f6e5f780ed
Vyacheslav <kokorinv19@gmail.com>
2021-09-29 23:00:30 +0500

    removed excessive tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,10 @@ import { test, expect } from '@jest/globals';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import comparingTree1 from '../valuesForTests/forJSONcompar1.js';
-import comparingTree2 from '../valuesForTests/forJSONcompar2.js';
-import comparingYamlTree from '../valuesForTests/forYAMLcompar.js';
-import jsonOutput from '../valuesForTests/JSONoutput.js';
-import * as plain from '../valuesForTests/PlainOutput.js';
+import comparingTree from '../__fixtures__/forJSONcompar.js';
+import comparingYamlTree from '../__fixtures__/forYAMLcompar.js';
+import jsonOutput from '../__fixtures__/JSONoutput.js';
+import plainOutput from '../__fixtures__/PlainOutput.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,14 +14,9 @@ const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', 
 
 test.each([
   {
-    readFirstFile: getFixturePath('file1.json'),
-    readSecondFile: getFixturePath('file2.json'),
-    expected: comparingTree1,
-  },
-  {
     readFirstFile: getFixturePath('file3.json'),
     readSecondFile: getFixturePath('file4.json'),
-    expected: comparingTree2,
+    expected: comparingTree,
   },
 ])('comparing not flat json files (build tree)', ({ readFirstFile, readSecondFile, expected }) => {
   expect(genDiff(readFirstFile, readSecondFile, 'stylish')).toBe(expected);
@@ -34,19 +28,14 @@ test('comparing not flat yaml files (build tree)', () => {
 
 test.each([
   {
-    readFirstFile: getFixturePath('file1.json'),
-    readSecondFile: getFixturePath('file2.json'),
-    expected: plain.plainOutput1,
-  },
-  {
     readFirstFile: getFixturePath('file3.json'),
     readSecondFile: getFixturePath('file4.json'),
-    expected: plain.plainOutput2,
+    expected: plainOutput,
   },
 ])('comparing not flat json files (plain output)', ({ readFirstFile, readSecondFile, expected }) => {
   expect(genDiff(readFirstFile, readSecondFile, 'plain')).toBe(expected);
 });
 
 test('comparing not flat json files (JSON output)', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(jsonOutput);
+  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'json')).toEqual(jsonOutput);
 });
