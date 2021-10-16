971b830e129456a39929bf912a9acd6eca3f6e7f
Vyacheslav <kokorinv19@gmail.com>
2021-10-16 13:09:23 +0500

    modified getStylish function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,35 +1,38 @@
+import _ from 'lodash';
+
+const getTreeElement = (elementName, symbol, elementValue, depth, replacer = '  ', spacesCount = 2) => {
+  const space = replacer.repeat(spacesCount * depth);
+  if (!_.isObject(elementValue)) {
+    return `\n${space.slice(2)}${symbol} ${elementName}: ${elementValue}`;
+  }
+  const entries = _.entries(elementValue);
+  const result = entries.map(([key, value]) => {
+    if (!_.isObject(value)) {
+      return `\n${space}    ${key}: ${elementValue[key]}`;
+    }
+    return `${getTreeElement(key, ' ', value, depth + 1)}`;
+  }).join('');
+  return `\n${space.slice(2)}${symbol} ${elementName}: {${result}\n${space}}`;
+};
+
 const getStylish = (innerTree, replacer = '  ', spacesCount = 2) => {
   const iter = (currentDiffValue, depth) => {
-    const firstSpace = replacer.repeat(spacesCount * depth);
-    const secondSpace = replacer.repeat(spacesCount * (depth - 1));
-    const makeString = (element, symbol, value) => `\n${firstSpace.slice(2)}${symbol} ${element.name}: ${element[value]}`;
     const resultString = currentDiffValue.map((element) => {
-      if (element.condition === 'not changed' || element.nodeCondition === 'not changed') {
-        return (element.nodeCondition === 'not changed')
-          ? [`\n${firstSpace}${element.name}: ${iter(element.children, depth + 1)}`]
-          : [makeString(element, ' ', 'oldValue')];
+      if (element.type === 'nested') {
+        return `\n${replacer.repeat(spacesCount * depth)}${element.name}: ${iter(element.children, depth + 1)}`;
       }
-      if (element.condition === 'added' || element.nodeCondition === 'added') {
-        return (element.nodeCondition === 'added')
-          ? [`\n${firstSpace.slice(2)}+ ${element.name}: ${iter(element.children, depth + 1)}`]
-          : [makeString(element, '+', 'newValue')];
+      if (element.type === 'deleted') {
+        return getTreeElement(element.name, '-', element.oldValue, depth);
       }
-      if (element.condition === 'deleted' || element.nodeCondition === 'deleted') {
-        return (element.nodeCondition === 'deleted')
-          ? [`\n${firstSpace.slice(2)}- ${element.name}: ${iter(element.children, depth + 1)}`]
-          : [makeString(element, '-', 'oldValue')];
+      if (element.type === 'added') {
+        return getTreeElement(element.name, '+', element.newValue, depth);
       }
-      if (element.nodeCondition === 'updated to str') {
-        return [`\n${firstSpace.slice(2)}- ${element.name}: ${iter(element.children, depth + 1)}`,
-          makeString(element, '+', 'newValue')].join('');
+      if (element.type === 'unchanged') {
+        return getTreeElement(element.name, ' ', element.oldValue, depth);
       }
-      if (element.nodeCondition === 'updated to obj') {
-        return [makeString(element, '-', 'oldValue'),
-          `\n${firstSpace.slice(2)}+ ${element.name}: ${iter(element.children, depth + 1)}`].join('');
-      }
-      return `${makeString(element, '-', 'oldValue')}${makeString(element, '+', 'newValue')}`;
+      return `${getTreeElement(element.name, '-', element.oldValue, depth)}${getTreeElement(element.name, '+', element.newValue, depth)}`;
     });
-    return ['{', ...resultString, `\n${secondSpace}}`].join('');
+    return ['{', ...resultString, `\n${replacer.repeat(spacesCount * (depth - 1))}}`].join('');
   };
   return iter(innerTree, 1);
 };
