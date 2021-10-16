9807c301112f65e5b85e10eeae12b05d82c23df1
Vyacheslav <kokorinv19@gmail.com>
2021-10-16 13:16:47 +0500

    delete comments

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,44 +1,5 @@
 import _ from 'lodash';
 
-// const getValue = (value) => {
-//   if (typeof value === 'string') {
-//     return `'${value}'`;
-//   }
-//   if (_.isObject(value)) {
-//     return '[complex value]';
-//   }
-//   return value;
-// };
-
-// const getPlain = (innerTree) => {
-//   const iter = (innerDiff, path) => {
-//     const plainResult = innerDiff
-//       .filter((element) => element.condition !== 'not changed')
-//       .map((element) => {
-//         const nodeName = `${path}${element.name}`;
-//         if (element.condition === 'changed'
-//           || element.nodeCondition === 'updated to str'
-//           || element.nodeCondition === 'updated to obj'
-//         ) {
-//           return `Property '${nodeName}' was updated. From ${getValue(element.oldValue)} to ${getValue(element.newValue)}`;
-//         }
-//         if (element.condition === 'deleted' || element.nodeCondition === 'deleted') {
-//           return `Property '${nodeName}' was removed`;
-//         }
-//         if (element.condition === 'added' || element.nodeCondition === 'added') {
-//           return `Property '${nodeName}' was added with value: ${getValue(element.newValue)}`;
-//         }
-//         if (_.has(element, 'nodeCondition')) {
-//           return `${iter(element.children, `${nodeName}.`)}`;
-//         }
-//         return `${element.name}`;
-//       });
-
-//     return plainResult.join('\n');
-//   };
-//   return iter(innerTree, '');
-// };
-
 const getValue = (value) => {
   if (typeof value === 'string') {
     return `'${value}'`;
