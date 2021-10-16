19cb4d980184714051ee9c1ca84248b10e9f05cd
Vyacheslav <kokorinv19@gmail.com>
2021-10-16 13:08:27 +0500

    modified getPlain function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,44 @@
 import _ from 'lodash';
 
+// const getValue = (value) => {
+//   if (typeof value === 'string') {
+//     return `'${value}'`;
+//   }
+//   if (_.isObject(value)) {
+//     return '[complex value]';
+//   }
+//   return value;
+// };
+
+// const getPlain = (innerTree) => {
+//   const iter = (innerDiff, path) => {
+//     const plainResult = innerDiff
+//       .filter((element) => element.condition !== 'not changed')
+//       .map((element) => {
+//         const nodeName = `${path}${element.name}`;
+//         if (element.condition === 'changed'
+//           || element.nodeCondition === 'updated to str'
+//           || element.nodeCondition === 'updated to obj'
+//         ) {
+//           return `Property '${nodeName}' was updated. From ${getValue(element.oldValue)} to ${getValue(element.newValue)}`;
+//         }
+//         if (element.condition === 'deleted' || element.nodeCondition === 'deleted') {
+//           return `Property '${nodeName}' was removed`;
+//         }
+//         if (element.condition === 'added' || element.nodeCondition === 'added') {
+//           return `Property '${nodeName}' was added with value: ${getValue(element.newValue)}`;
+//         }
+//         if (_.has(element, 'nodeCondition')) {
+//           return `${iter(element.children, `${nodeName}.`)}`;
+//         }
+//         return `${element.name}`;
+//       });
+
+//     return plainResult.join('\n');
+//   };
+//   return iter(innerTree, '');
+// };
+
 const getValue = (value) => {
   if (typeof value === 'string') {
     return `'${value}'`;
@@ -13,23 +52,20 @@ const getValue = (value) => {
 const getPlain = (innerTree) => {
   const iter = (innerDiff, path) => {
     const plainResult = innerDiff
-      .filter((element) => element.condition !== 'not changed')
+      .filter((element) => element.type !== 'unchanged')
       .map((element) => {
         const nodeName = `${path}${element.name}`;
-        if (element.condition === 'changed'
-          || element.nodeCondition === 'updated to str'
-          || element.nodeCondition === 'updated to obj'
-        ) {
-          return `Property '${nodeName}' was updated. From ${getValue(element.oldValue)} to ${getValue(element.newValue)}`;
+        if (element.type === 'nested') {
+          return `${iter(element.children, `${nodeName}.`)}`;
         }
-        if (element.condition === 'deleted' || element.nodeCondition === 'deleted') {
+        if (element.type === 'deleted') {
           return `Property '${nodeName}' was removed`;
         }
-        if (element.condition === 'added' || element.nodeCondition === 'added') {
+        if (element.type === 'added') {
           return `Property '${nodeName}' was added with value: ${getValue(element.newValue)}`;
         }
-        if (_.has(element, 'nodeCondition')) {
-          return `${iter(element.children, `${nodeName}.`)}`;
+        if (element.type === 'changed') {
+          return `Property '${nodeName}' was updated. From ${getValue(element.oldValue)} to ${getValue(element.newValue)}`;
         }
         return `${element.name}`;
       });
