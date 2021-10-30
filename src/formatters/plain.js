72e2ab2360e63adcf4a8a628de00990697c0ef93
Vyacheslav <kokorinv19@gmail.com>
2021-10-31 00:12:12 +0500

    modified plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,19 +16,20 @@ const getPlain = (innerTree) => {
       .filter((element) => element.type !== 'unchanged')
       .map((element) => {
         const nodeName = `${path}${element.name}`;
-        if (element.type === 'nested') {
-          return `${iter(element.children, `${nodeName}.`)}`;
+        switch (element.type) {
+          case 'nested':
+            return `${iter(element.children, `${nodeName}.`)}`;
+          case 'deleted':
+            return `Property '${nodeName}' was removed`;
+          case 'added':
+            return `Property '${nodeName}' was added with value: ${getValue(element.value)}`;
+          case 'changed':
+            return `Property '${nodeName}' was updated. From ${getValue(element.value1)} to ${getValue(element.value2)}`;
+          case 'unchanged':
+            return `${element.name}`;
+          default:
+            throw new Error(`Element type ${element.type} is not supported!`);
         }
-        if (element.type === 'deleted') {
-          return `Property '${nodeName}' was removed`;
-        }
-        if (element.type === 'added') {
-          return `Property '${nodeName}' was added with value: ${getValue(element.value)}`;
-        }
-        if (element.type === 'changed') {
-          return `Property '${nodeName}' was updated. From ${getValue(element.value1)} to ${getValue(element.value2)}`;
-        }
-        return `${element.name}`;
       });
 
     return plainResult.join('\n');
