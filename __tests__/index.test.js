5413bc3af155c042367c2548414484410e47eb4c
Vyacheslav <kokorinv19@gmail.com>
2021-11-16 22:50:25 +0500

    modified tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,11 +11,11 @@ const buildFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__'
 const readFile = (filename) => fs.readFileSync(buildFixturePath(filename), 'utf-8');
 
 test.each([
-  { expected: 'stylishOutput.txt', format: 'stylish' },
-  { expected: 'plainOutput.txt', format: 'plain' },
-  { expected: 'JSONOutput.txt', format: 'json' },
-])('comparing not flat json and yaml files', ({ expected, format }) => {
+  { format: 'stylish' },
+  { format: 'plain' },
+  { format: 'json' },
+])('comparing not flat json and yaml files', ({ format }) => {
   const file1 = buildFixturePath('file1.json');
   const file2 = buildFixturePath('file2.yaml');
-  expect(genDiff(file1, file2, format)).toBe(readFile(expected));
+  expect(genDiff(file1, file2, format)).toBe(readFile(`${format}Output.txt`));
 });
